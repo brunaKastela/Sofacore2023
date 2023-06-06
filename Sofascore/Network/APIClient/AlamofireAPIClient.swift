8ac52c736f7e001fa2e9ae6905106e528d10b3ce
Bruna <bruna.kastela@gmail.com>
2023-06-06 22:48:33 +0200

    Safety check before fetching data

--- Sofascore/Network/APIClient/AlamofireAPIClient.swift
+++ Sofascore/Network/APIClient/AlamofireAPIClient.swift
@@ -5,7 +5,7 @@ class AlamofireAPIClient: APICient {
 
     func getEvents(for slug: String, for date: String, completion: @escaping ([Event?], Error?) -> Void) {
 
-        // later check format for slug and date
+        guard isValidDateFormat(date) && isValidSlug(slug) else { return }
 
         AF
 //            .request("https://academy.dev.sofascore.com/sport/football/events/2023-04-29")
@@ -17,4 +17,28 @@ class AlamofireAPIClient: APICient {
 
 }
 
-// Izdvojiti sve urlove negdi?
+extension AlamofireAPIClient {
+
+    private enum Constants: String, CaseIterable {
+        case football = "football"
+        case basketball = "basketball"
+        case amFootball = "american-football"
+    }
+
+    func isValidDateFormat(_ date: String) -> Bool {
+        do {
+            let pattern = #"^\d{4}-\d{2}-\d{2}$"#
+            let regex = try NSRegularExpression(pattern: pattern, options: [])
+            let range = NSRange(location: 0, length: date.utf16.count)
+            let matches = regex.matches(in: date, options: [], range: range)
+
+            return matches.count == 1
+        } catch {
+            return false
+        }
+    }
+
+    func isValidSlug(_ slug: String) -> Bool {
+        Constants.allCases.map { $0.rawValue }.contains(slug)
+    }
+}
